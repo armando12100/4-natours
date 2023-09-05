f5a2dfcf916871d16e1f18b6ec8a82fed53c0499
Armando Sanchez <asan5900@gmail.com>
2023-09-05 00:05:15 -0700

    2nd Commit

--- server.js
+++ server.js
@@ -1,7 +1,42 @@
+const mongoose = require('mongoose');
 const app = require('./app');
-const dotenv = require('./dotenv');
+const dotenv = require('dotenv');
 
-dotenv.config({path: './config.env'})
+dotenv.config({path: './config.env'});
+
+const DB = process.env.DATABASE
+
+mongoose.connect(DB);
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
+    }
+})
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+    name: 'The Forest Hiker Part Deaux',
+    rating: 4.7,
+    price: 497
+});
+
+testTour.save().then(doc => {
+    console.log(doc);
+}).catch(err => {
+    console.log('ERROR', err)
+});
 
 const port = 3000;
 app.listen(port, () => {
